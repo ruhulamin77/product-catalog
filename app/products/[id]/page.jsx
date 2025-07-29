597f30141934dea675c60eb45035001f355e6e7f
ruhulamin77 <ruhulaminbd93@gmail.com>
2025-07-30 00:34:56 +0600

    remove add to cart form ui

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -56,9 +56,6 @@ export default async function ProductDetailsPage({ params }) {
             >
               Back to Home
             </Link>
-            <button className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-              Add to Cart
-            </button>
           </div>
         </div>
       </div>
